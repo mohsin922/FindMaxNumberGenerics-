6d76c74f16c6872b79128eac2c875d998e3b3dbc
unknown <mohsinpeer22@gmail.com>
2022-01-01 11:58:52 +0530

    Finding MaxNumber using Generics

---
+++ FindMaxNumberGenerics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FindMaxNumberGenerics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Finding MaxNumber using Generics!");
+        }
+    }
+}
