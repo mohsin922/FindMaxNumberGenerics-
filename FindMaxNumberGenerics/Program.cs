43e985fb55a630d15e67f2e1e43b75bb8710d031
unknown <mohsinpeer22@gmail.com>
2022-01-01 12:22:37 +0530

    Max Of Three Integers

--- FindMaxNumberGenerics/Program.cs
+++ FindMaxNumberGenerics/Program.cs
@@ -4,9 +4,31 @@ namespace FindMaxNumberGenerics
 {
     class Program
     {
+        public int FindMaximum(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)
+            {
+                return first;
+            }
+            else if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            else if (third.CompareTo(second) > 0 && third.CompareTo(first) > 0)
+            {
+                return third;
+            }
+            else
+            {
+                Console.WriteLine("Numbers are same");
+                return 1;
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Finding MaxNumber using Generics!");
+            Program obj = new Program();
+            int maxValue = obj.FindMaximum(120, 35, 645);
+            Console.WriteLine("Maximum of three values is : {0}", maxValue);
         }
     }
 }
